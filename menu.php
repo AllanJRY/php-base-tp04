c111810d49e084f7ccb5ce8869e3af774ac92c5f
Allan JARRY <jarry.allan@gmail.com>
2022-11-23 14:07:46 +0100

    Exercice 4

--- menu.php
+++ menu.php
@@ -1,5 +1,11 @@
 <ul>
-    <li><a href="index.php">Accueil</a></li>
-    <li><a href="tarifs.php">Tarifs</a></li>
-    <li><a href="contact.php">Contact</a></li>
+    <?php if($page !== 'Accueil') { ?>
+        <li><a href="index.php">Accueil</a></li>
+    <?php } ?>
+    <?php if($page !== 'Tarifs') { ?>
+        <li><a href="tarifs.php">Tarifs</a></li>
+    <?php } ?>
+    <?php if($page !== 'Contact') { ?>
+        <li><a href="contact.php">Contact</a></li>
+    <?php } ?>
 </ul>
